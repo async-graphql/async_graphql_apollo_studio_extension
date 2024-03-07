d5df877b6efcaf981f6735d216323351ef8255b5
Shashi Kant <shashitnak@gmail.com>
2024-03-07 10:50:59 +0530

    fixing serde and libflate

--- src/compression.rs
+++ src/compression.rs
@@ -1,10 +1,10 @@
-#[cfg(feature = "compression")]
+#[cfg(all(feature = "compression", not(target_arch = "wasm32")))]
 use libflate::gzip;
 
-#[cfg(feature = "compression")]
+#[cfg(all(feature = "compression", not(target_arch = "wasm32")))]
 const TARGET_LOG_COMPRESSION: &str = "apollo-studio-extension-compression";
 
-#[cfg(feature = "compression")]
+#[cfg(all(feature = "compression", not(target_arch = "wasm32")))]
 pub fn compress(msg: Vec<u8>) -> Result<Vec<u8>, std::io::Error> {
     let mut encoder = gzip::Encoder::new(Vec::new()).unwrap();
     let mut msg = std::io::Cursor::new(msg);
@@ -20,7 +20,7 @@ pub fn compress(msg: Vec<u8>) -> Result<Vec<u8>, std::io::Error> {
     encoder.finish().into_result()
 }
 
-#[cfg(not(feature = "compression"))]
+#[cfg(any(not(feature = "compression"), target_arch = "wasm32"))]
 pub fn compress(msg: Vec<u8>) -> Result<Vec<u8>, std::io::Error> {
     Ok::<Vec<u8>, std::io::Error>(msg)
 }
