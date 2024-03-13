395669ba92390d68a63a55f1e7aa0495d19d6cb0
Shashi Kant <shashitnak@gmail.com>
2024-03-13 21:31:17 +0530

    skipping uname call for windows too

--- src/packages/uname.rs
+++ src/packages/uname.rs
@@ -1,18 +1,20 @@
 
-#[cfg(not(target_arch = "wasm32"))]
-pub fn uname() -> std::io::Result<String> {
-    let x = uname::uname()?;
-    Ok(format!(
-        "{sysname} {version} {release} {machine} {nodename}",
-        sysname = x.sysname,
-        version = x.version,
-        release = x.release,
-        machine = x.machine,
-        nodename = x.nodename
-    ))
-}
-
-#[cfg(target_arch = "wasm32")]
-pub fn uname() -> std::io::Result<String> {
-    Err(std::io::Error::new(std::io::ErrorKind::NotFound, "not supported on wasm32"))
+cfg_if::cfg_if! {
+    if #[cfg(all(target_arch = "wasm32", target_os = "windows"))] {
+        pub fn uname() -> std::io::Result<String> {
+            Err(std::io::Error::new(std::io::ErrorKind::NotFound, "not supported on wasm32"))
+        }
+    } else {
+         pub fn uname() -> std::io::Result<String> {
+            let x = uname::uname()?;
+            Ok(format!(
+                "{sysname} {version} {release} {machine} {nodename}",
+                sysname = x.sysname,
+                version = x.version,
+                release = x.release,
+                machine = x.machine,
+                nodename = x.nodename
+            ))
+        }
+    }
 }
